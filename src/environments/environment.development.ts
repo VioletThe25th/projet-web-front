6596ad24155517ae8f139be87afc3338174be3c1
Jeremy Bilger <64845106+VioletThe25th@users.noreply.github.com>
2023-03-07 10:50:27 +0100

    installed all the thing to setup the back

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'web-front-d58bf',
+    appId: '1:580507917259:web:f21c73fb506e6db29cf44e',
+    storageBucket: 'web-front-d58bf.appspot.com',
+    apiKey: 'AIzaSyC9aa37R2UkgN7hV5dveuYAtEacKGZpXrc',
+    authDomain: 'web-front-d58bf.firebaseapp.com',
+    messagingSenderId: '580507917259',
+    measurementId: 'G-SKSN2MEHCD',
+  },};
